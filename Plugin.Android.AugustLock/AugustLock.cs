9dbf939dc257b3bb42a3ea3ac62ab246378cab7f
Marcus Lum <marcusl@gmail.com>
2017-03-17 14:47:35 -0500

    Add project files and nuspec.

---
+++ Plugin.Android.AugustLock/AugustLock.cs
@@ -0,0 +1,275 @@
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Plugin.Android.AugustLock
+{
+    // ported from https://github.com/ethitter/augustctl/blob/master/lib/lock.js
+    public class AugustLockDevice
+    {
+        private Guid AUGUST_SERVICE = new Guid("bd4ac610-0b45-11e3-8ffd-0800200c9a66");
+        private LockSession lock_session;
+        private SecureLockSession secure_lock_session;
+        private bool isSecure;
+        private IDevice augustLock;
+
+        // public properties
+        public Guid ID { get; private set; }
+        public string OfflineKey { get; private set; }
+        public int OfflineKeyOffset { get; private set; }
+
+        // events
+        public event EventHandler<string> DebugMessage;
+
+        public AugustLockDevice(string id, string offlineKey, int offlineKeyOffset)
+        {
+            if (id == "0")
+            {
+                id = "000000000000";
+            }
+            ID = new Guid("00000000-0000-0000-0000-" + id);
+
+            OfflineKey = offlineKey;
+            OfflineKeyOffset = offlineKeyOffset;
+        }
+
+        internal void Debug(string log)
+        {
+            DebugMessage?.Invoke(this, log);
+        }
+
+        public async Task<bool> Connect()
+        {
+            try
+            {
+                augustLock = null;
+                var adapter = CrossBluetoothLE.Current.Adapter;
+                var tokenSource = new CancellationTokenSource();
+                EventHandler<DeviceEventArgs> lockDiscovered = (s, a) =>
+                {
+                    augustLock = a.Device;
+                    tokenSource.Cancel();
+                };
+                try
+                {
+                    adapter.DeviceDiscovered += lockDiscovered;
+                    var scanTask = adapter.StartScanningForDevicesAsync(
+                        new[] { AUGUST_SERVICE }, device => device.Id == ID || ID.Equals(Guid.Empty), false, tokenSource.Token);
+
+                    if (await Task.WhenAny(scanTask, Task.Delay(5000)) == scanTask)
+                    {
+                        await scanTask;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug("scan exceptioned: " + ex.GetType().ToString() + ":" + ex.Message + "\n" + ex.StackTrace);
+                }
+                finally
+                {
+                    adapter.DeviceDiscovered -= lockDiscovered; // detach handler
+                    try
+                    {
+                        await adapter.StopScanningForDevicesAsync();
+                    }
+                    catch (Exception iex)
+                    {
+                        Debug("Could not stop scanning for devices: " + iex.Message);
+                    }
+                }
+                if (augustLock != null)
+                {
+                    var connectTask = adapter.ConnectToDeviceAsync(augustLock);
+                    if (await Task.WhenAny(new[] { connectTask, Task.Delay(5000) }) == connectTask)
+                    {
+                        await connectTask; // in case the task exceptioned out to throw the exception
+                        await SetupSessions(augustLock);
+                        await PerformHandshake();
+                        return true;
+                    }
+                }
+                else
+                {
+                    Debug("august lock not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug("connect error: " + ex.Message);
+            }
+            return false;
+        }
+
+        private byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length / 2).Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16)).ToArray();
+        }
+
+        private async Task SetupSessions(IDevice peripheral)
+        {
+            Debug("connected to lock: " + peripheral.Name + ">" + peripheral.Id);
+            var service = await peripheral.GetServiceAsync(AUGUST_SERVICE);
+
+            secure_lock_session = new SecureLockSession(this)
+            {
+                OfflineKeyOffset = (byte)OfflineKeyOffset,
+                ReadCharacteristic = await service.GetCharacteristicAsync(new Guid("bd4ac614-0b45-11e3-8ffd-0800200c9a66")),
+                WriteCharacteristic = await service.GetCharacteristicAsync(new Guid("bd4ac613-0b45-11e3-8ffd-0800200c9a66"))
+            };
+            secure_lock_session.SetKey(StringToByteArray(OfflineKey));
+            lock_session = new LockSession(this)
+            {
+                ReadCharacteristic = await service.GetCharacteristicAsync(new Guid("bd4ac612-0b45-11e3-8ffd-0800200c9a66")),
+                WriteCharacteristic = await service.GetCharacteristicAsync(new Guid("bd4ac611-0b45-11e3-8ffd-0800200c9a66"))
+            };
+
+            // start up pipes
+            await secure_lock_session.Start();
+            await lock_session.Start();
+        }
+
+        private async Task PerformHandshake()
+        {
+            var handshakeKeys = new byte[16];
+            RandomNumberGenerator.Create().GetBytes(handshakeKeys);
+
+            var cmd = secure_lock_session.BuildCommand(0x01);
+            Array.Copy(handshakeKeys, 0, cmd, 4, 8);
+            var response = await secure_lock_session.Execute(cmd);
+            if (response[0] != 0x02)
+            {
+                throw new Exception("unexpected response to SEC_LOCK_TO_MOBILE_KEY_EXCHANGE: " + BitConverter.ToString(response));
+            }
+            Debug("handshake complete");
+
+            // secure session established
+            isSecure = true;
+
+            // setup the session key
+            var sessionKey = new byte[16];
+            Array.Copy(handshakeKeys, 0, sessionKey, 0, 8);
+            Array.Copy(response, 4, sessionKey, 8, 8);
+            lock_session.SetKey(sessionKey);
+
+            // rekey the secure session as well
+            secure_lock_session.SetKey(sessionKey);
+
+            // send SEC_INITIALIZATION_COMMAND
+            cmd = secure_lock_session.BuildCommand(0x03);
+            Array.Copy(handshakeKeys, 8, cmd, 4, 8);
+            response = await secure_lock_session.Execute(cmd);
+            if (response[0] != 0x04)
+            {
+                throw new Exception("unexpected response to SEC_INITIALIZATION_COMMAND: " + BitConverter.ToString(response));
+            }
+            Debug("lock initialized");
+        }
+
+        public async Task ForceLock()
+        {
+            Debug("locking...");
+            var cmd = lock_session.BuildCommand(0x0b);
+            await this.lock_session.Execute(cmd);
+        }
+
+        public async Task ForceUnlock()
+        {
+            Debug("unlocking...");
+            var cmd = lock_session.BuildCommand(0x0a);
+            await lock_session.Execute(cmd);
+        }
+
+        public async Task Lock()
+        {
+            if (await this.Status() == "unlocked")
+            {
+                await this.ForceLock();
+            }
+        }
+
+        public async Task Unlock()
+        {
+            if (await this.Status() == "locked")
+            {
+                await this.ForceUnlock();
+            }
+        }
+
+        public async Task Toggle()
+        {
+            switch (await Status())
+            {
+                case "locked":
+                    await ForceUnlock();
+                    break;
+                case "unlocked":
+                    await ForceLock();
+                    break;
+            }
+        }
+
+        public async Task<string> Status()
+        {
+            Debug("status...");
+            var cmd = new byte[18];
+            cmd[0] = 0xee; // magic
+            cmd[1] = 0x02;
+            cmd[4] = 0x02;
+            cmd[16] = 0x02;
+
+            try
+            {
+                var response = await lock_session.Execute(cmd);
+                var status = response[8];
+
+                var strstatus = "unknown";
+                if (status == 0x03)
+                    strstatus = "unlocked";
+                else if (status == 0x05)
+                    strstatus = "locked";
+
+                Debug(strstatus);
+                return strstatus;
+            }
+            catch (Exception ex)
+            {
+                Debug(ex.Message + "\n" + ex.StackTrace);
+                return "error";
+            }
+        }
+
+        public async Task Disconnect()
+        {
+            Debug("disconnecting...");
+            if (lock_session != null)
+            {
+                Func<Task> disconnect = async () =>
+                {
+                    var adapter = CrossBluetoothLE.Current.Adapter;
+                    await adapter.DisconnectDeviceAsync(augustLock);
+                };
+
+                if (isSecure)
+                {
+                    isSecure = false;
+                    var cmd = secure_lock_session.BuildCommand(0x05);
+                    cmd[17] = 0x00;
+                    var response = await secure_lock_session.Execute(cmd);
+                    if (response[0] != 0x8b)
+                    {
+                        throw new Exception("unexpected response to DISCONNECT: " + BitConverter.ToString(response));
+                    }
+                }
+                else
+                {
+                    await disconnect();
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.Android.AugustLock/Sessions.cs
@@ -0,0 +1,200 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Plugin.Android.AugustLock
+{
+    // ported from https://github.com/ethitter/augustctl/blob/master/lib/lock_session.js
+    class LockSession
+    {
+        private AugustLockDevice device;
+        private ICryptoTransform encryptCipher;
+        private ICryptoTransform decryptCipher;
+        protected CipherMode cipherMode = CipherMode.CBC;
+        protected byte[] iv = new byte[16];
+        private byte[] readData;
+        private ManualResetEvent readEvent = new ManualResetEvent(true);
+
+        public ICharacteristic WriteCharacteristic { get; set; }
+        public ICharacteristic ReadCharacteristic { get; set; }
+
+        public LockSession(AugustLockDevice parent)
+        {
+            device = parent;
+        }
+
+        public void SetKey(byte[] key)
+        {
+            var aes = new AesManaged();
+            aes.Mode = cipherMode;
+            aes.Padding = PaddingMode.None;
+            aes.Key = key;
+            aes.IV = this.iv;
+            encryptCipher = aes.CreateEncryptor();
+            decryptCipher = aes.CreateDecryptor();
+        }
+
+        public async Task Start()
+        {
+            ReadCharacteristic.ValueUpdated += (obj, args) =>
+            {
+                var buf = args.Characteristic.Value;
+                readData = new byte[buf.Length];
+                buf.CopyTo(readData, 0);
+
+                if (decryptCipher != null)
+                {
+                    device.Debug("encrypted data: " + BitConverter.ToString(readData));
+                    decryptCipher.TransformBlock(readData, 0, 16, readData, 0);
+                }
+                device.Debug("received data: " + BitConverter.ToString(readData));
+                readEvent.Set();
+            };
+
+            var sTask = ReadCharacteristic.StartUpdatesAsync();
+            if (await Task.WhenAny(sTask, Task.Delay(5000)) == sTask)
+            {
+                await sTask;
+            }
+            else
+            {
+                throw new Exception("Read listener startup timeout");
+            }
+        }
+
+        public virtual byte[] BuildCommand(byte opcode)
+        {
+            var cmd = new byte[18];
+            cmd[0] = 0xee; // magic
+            cmd[1] = opcode;
+            cmd[16] = 0x02; // unknown
+            return cmd;
+        }
+
+        public int SimpleChecksum(byte[] buf)
+        {
+            int cs = 0;
+            for (int i = 0; i < 18; i++)
+            {
+                cs = (cs + buf[i]) & 0xff;
+            }
+            return (-cs) & 0xff;
+        }
+
+        protected virtual void WriteChecksum(byte[] command)
+        {
+            command[3] = (byte)SimpleChecksum(command);
+        }
+
+        protected virtual void ValidateResponse(byte[] response)
+        {
+            if (SimpleChecksum(response) != 0)
+            {
+                throw new Exception("simple checksum mismatch");
+            }
+            if (response[0] != 0xbb && response[0] != 0xaa)
+            {
+                throw new Exception("unexpected magic in response");
+            }
+        }
+
+        private async Task<bool> Write(byte[] command)
+        {
+            // NOTE: the last two bytes are not encrypted
+            // general idea seems to be that if the last byte of the command indicates an offline key offset (is non-zero), the command is "secure" and encrypted with the offline key
+            if (encryptCipher != null)
+            {
+                encryptCipher.TransformBlock(command, 0, 16, command, 0);
+                device.Debug("write (encrypted): " + BitConverter.ToString(command));
+            }
+            // write the command to the write characteristic
+            return await WriteCharacteristic.WriteAsync(command);
+        }
+
+        public async Task<byte[]> Execute(byte[] command)
+        {
+            // reset the wait event
+            readEvent.Reset();
+            WriteChecksum(command);
+            device.Debug("execute command: " + BitConverter.ToString(command));
+            var wTask = Write(command);
+            if (await Task.WhenAny(wTask, Task.Delay(2000)) == wTask)
+            {
+                await wTask;
+            }
+            else
+            {
+                throw new Exception("execution timeout");
+            }
+            device.Debug("write successful");
+
+            return await Task.Run<byte[]>(() =>
+            {
+                // process response
+                if (!readEvent.WaitOne(TimeSpan.FromSeconds(5)))
+                {
+                    throw new Exception("Write response timeout");
+                }
+                byte[] data = new byte[readData.Length];
+                readData.CopyTo(data, 0);
+                ValidateResponse(data);
+                return data;
+            });
+
+        }
+    }
+
+    // ported from https://github.com/ethitter/augustctl/blob/master/lib/secure_lock_session.js
+    class SecureLockSession : LockSession
+    {
+        public byte OfflineKeyOffset { get; set; }
+
+        public SecureLockSession(AugustLockDevice parent) : base(parent)
+        {
+            cipherMode = CipherMode.ECB;
+        }
+
+        public override byte[] BuildCommand(byte opcode)
+        {
+            var cmd = new byte[18];
+            cmd[0] = opcode;
+            cmd[16] = 0x0f; // unknown
+            cmd[17] = OfflineKeyOffset;
+            return cmd;
+        }
+
+        private UInt32 ReadUInt32LE(byte[] buffer, int startIndex)
+        {
+            var buf = new byte[4];
+            Array.Copy(buffer, startIndex, buf, 0, 4);
+            return BitConverter.ToUInt32(buf, 0);
+        }
+
+        private void WriteUInt32LE(byte[] buffer, uint value, int startIndex)
+        {
+            var buf = BitConverter.GetBytes(value);
+            Array.Copy(buf, 0, buffer, startIndex, 4);
+        }
+
+        public uint SecurityChecksum(byte[] buffer)
+        {
+            return (0 - (ReadUInt32LE(buffer, 0x00) + ReadUInt32LE(buffer, 0x04) + ReadUInt32LE(buffer, 0x08))) >> 0;
+        }
+
+        protected override void WriteChecksum(byte[] command)
+        {
+            var cs = SecurityChecksum(command);
+            WriteUInt32LE(command, cs, 0x0c);
+        }
+
+        protected override void ValidateResponse(byte[] data)
+        {
+            if (SecurityChecksum(data) != ReadUInt32LE(data, 0x0c))
+            {
+                throw new Exception("security checksum mismatch");
+            }
+        }
+    }
+}
